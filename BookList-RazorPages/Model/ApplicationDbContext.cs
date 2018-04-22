d361e0ba0ff4d141ddca32fc672024e05c8d5e2c
Manuel Rey <01manuelrey@gmail.com>
2018-04-22 00:15:49 -0400

    Add functionality

---
+++ BookList-RazorPages/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_RazorPages.Model
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Db set for add books into database
+        public DbSet<Book> Books { get; set; }
+    }
+}
